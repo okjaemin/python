5a9e3c4bc51881ad3ef9ada6eadd234eb31ac8f5
okjaemin1120 <65061118+okjaemin1120@users.noreply.github.com>
2020-05-09 15:40:19 +0900

    Create age.py
    computing your age...

---
+++ age.py
@@ -0,0 +1,9 @@
+print('나이 계산 프로그램')
+name='옥재민'
+birth_year=2006
+current_year=2020
+age=current_year-birth_year+1
+target_age=20
+future_year=target_age-age+current_year
+print('당신은',name, '이고',birth_year,'년생이고 나이는', age,'살입니다.')
+print('당신은',future_year,'년에',target_age,'살이 됩니다.'  )
